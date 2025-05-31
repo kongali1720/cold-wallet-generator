a8b7268eead90e5b87a6ddc6a593670a4a1ddf44
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-05-31 23:53:42 +0700

    Create doge_wallet.py

---
+++ wallets/doge_wallet.py
@@ -0,0 +1,8 @@
+# wallets/doge_wallet.py
+
+# Placeholder (karena Dogecoin pakai Base58 + jaringan khusus)
+def generate_doge_wallet():
+    return {
+        'address': 'DogeAddress123ABC',
+        'private_key': 'PrivateKeyDogeABC'
+    }
