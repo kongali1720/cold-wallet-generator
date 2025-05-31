f64710c0eb39fbffb373538f4728d5fe9681f6b8
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-05-31 23:55:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # main.py
+
 from wallets.btc_wallet import generate_btc_wallet
+from wallets.eth_wallet import generate_eth_wallet
+from wallets.doge_wallet import generate_doge_wallet
+
 from utils.qr_generator import create_qr
 from utils.pdf_exporter import export_wallet_pdf
 
@@ -12,18 +16,22 @@ def main():
 
     if choice == '1':
         wallet = generate_btc_wallet()
+    elif choice == '2':
+        wallet = generate_eth_wallet()
+    elif choice == '3':
+        wallet = generate_doge_wallet()
     else:
-        print("Belum tersedia. Coming soon!")
+        print("Pilihan tidak valid.")
         return
 
     print("\n[+] Address:", wallet['address'])
     print("[+] Private Key:", wallet['private_key'])
 
-    # Buat QR
+    # Buat QR code
     create_qr(wallet['address'], 'address.png')
     create_qr(wallet['private_key'], 'private_key.png')
 
-    # Buat PDF
+    # Buat PDF backup
     export_wallet_pdf(wallet)
 
 if __name__ == "__main__":
